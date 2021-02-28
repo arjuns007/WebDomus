9103a058cc0bb87f5415d9b6b4a065056c585a85
mayank-gubba <mayank.gubba18@st.niituniversity.in>
2021-02-28 23:47:57 +0530

    Added teams Section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
+/* preloader*/
 $(window).on('load', function(){
     $('#status').delay(2000).fadeOut();
     $('#preloader').delay(2000).fadeOut();
+});
+
+/*team*/
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items : 2,
+        autoplay : true,
+        smartSpeed : 400,
+        loop: true,
+        autoplayHoverPause : true,
+        nav : true,
+        dots : false, 
+        navText : ['<i class="fas fa-angle-left"></i>' , '<i class="fas fa-angle-right"></i>']
+    });
+  });
+
+  /*progress bars*/
+
+$(function(){
+
+    $("#progress-elements").waypoint(function() {
+        $(".progress-bar").each(function(){
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") +"%"
+            }, 1000);
+        })
+        this.destroy();
+    }, {
+        offset:'bottom-in-view' 
+    });
 });
